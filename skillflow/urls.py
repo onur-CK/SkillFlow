a8fb978219109e136cd19e1b7f647b55b5534284
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-01-03 23:59:22 +0100

    import logout view urls.py

--- skillflow/urls.py
+++ skillflow/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
