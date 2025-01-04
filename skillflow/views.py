a9c4fdda488c485774f3be7090217dbe11d77a84
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-01-04 03:44:08 +0100

    create the password section for our login function.

--- skillflow/views.py
+++ skillflow/views.py
@@ -3,9 +3,6 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
-def login(request):
-    return render(request, 'skillflow/login.html')
-
 def about_us(request):
     return render(request, 'skillflow/about_us.html')
 
@@ -20,7 +17,15 @@ def sign_up(request):
         form = SignUpForm()
     return render(request, 'skillflow/sign_up.html', {'form': form})
         
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
     
+    return render(request, 'skillflow/login.html')    
+
+
 @login_required
 def index(request):
     return render(request, 'skillflow/index.html')
