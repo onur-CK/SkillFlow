d6b53e1da2b4903adaefe1f8e2ab0097cad67276
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-01-04 03:32:57 +0100

    Chhange the sign up function to work on it easier to fix the bug(cant display index.html after signing up)

--- skillflow/views.py
+++ skillflow/views.py
@@ -3,64 +3,29 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
-
-def edit_profile(request):
-    return render(request, 'skillflow/edit_profile.html')
-
-def index(request):
-    return render(request, 'skillflow/index.html')
-
 def login(request):
     return render(request, 'skillflow/login.html')
-
-def service(request):
-    return render(request, 'skillflow/service.html')
 
 def about_us(request):
     return render(request, 'skillflow/about_us.html')
 
 def sign_up(request):
-    # Check if the request is a POST request (form submitted by the user).
-    if request.method == 'POST': 
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirm = request.POST['confirm-password']
-        
-        if password != password_confirm:
-            return redirect('sign_up')
-        
-        if User.objects.filter(username=username).exists():
-            return redirect('sign_up')
-    
-        # Create a new user with the given username and password.
-        user = User.objects.create_user(username=username, password=password)
-        # Create a UserProfile instance for the newly created user.
-        UserProfile.objects.create(user=user)
-        login(request, user)
-        return redirect('index')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # Automatically log in user after signup
+            return redirect('index')  # Redirect to index page after successful signup
+    else:
+        form = SignUpForm()
+    return render(request, 'skillflow/sign_up.html', {'form': form})
         
     return render(request, 'skillflow/sign_up.html') 
     
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password'] 
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        
-    return render(request, 'skillflow/login.html')  
-
-
-
+  
 @login_required
 def index(request):
     return render(request, 'skillflow/index.html')
-
-def about_us(request):
-    return render(request, 'skillflow/about_us.html')
 
 def service(request):
     return render(request, 'skillflow/service.html')
