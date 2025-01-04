fcdabf8464151a2ec93d10d7185dbfc264436630
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-01-05 00:52:24 +0100

    Create userprofileform class bio field

--- skillflow/forms.py
+++ skillflow/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
+from .models import UserProfile, Service
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio']
 
