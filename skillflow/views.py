43265bfb8fbc330e0876ef758d0645dc57b01577
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-01-08 06:06:16 +0100

    Fix user_services underscore typo in views.py

--- skillflow/views.py
+++ skillflow/views.py
@@ -159,7 +159,7 @@ def my_services(request):
     # Django date query from newest to oldest link source: https://stackoverflow.com/questions/30314741/django-date-query-from-newest-to-oldest
     user_services = Service.objects.filter(provider=request.user).order_by('-created_at')
     return render(request, 'skillflow/my_services.html', {
-        'user-services': user_services
+        'user_services': user_services
     })
 
 
