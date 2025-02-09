e19c70fe623f9be186717bdc3cbae266ec02a86c
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-02-09 14:22:22 +0100

    Updated ALLOWED_HOSTS for Heroku deployment

--- skillflow/settings.py
+++ skillflow/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'skillflow-community-django.herokuapp.com', 'www.skillflow-community-django.herokuapp.com']
+
 
 
 # Application definition
