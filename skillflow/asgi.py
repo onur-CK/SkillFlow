fb274f2fc4c83a0f9242252b8ef126e5585eb1de
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-02-24 02:20:52 +0100

    Add comments and source link for asgi.py

--- skillflow/asgi.py
+++ skillflow/asgi.py
@@ -1,16 +1,27 @@
 """
 ASGI config for skillflow project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+ASGI (Asynchronous Server Gateway Interface) is a specification that describes
+how async-capable Python web servers, frameworks, and applications communicate
+with each other. This is the ASGI configuration for the skillflow project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+Key aspects:
+- Provides support for asynchronous request handling
+- Enables WebSocket support and other async protocols
+- Acts as an interface between the web server and Django application
 """
-
+# Source Link: https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
 
+# Set the Django settings module path for the ASGI application
+# This ensures Django knows which settings to use when running the application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillflow.settings')
 
+"""
+Initialize the ASGI application
+This creates an ASGI-compatible application object that web servers can use
+to communicate with Django application
+"""
 application = get_asgi_application()
